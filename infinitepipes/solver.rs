0823b28647ab41c2a5ae0f641c2d825bab9b5d17
Peixian Wang <peixian@fastmail.com>
2016-07-16 15:27:17 -0500

    Move to org file for organization

--- infinitepipes/solver.rs
+++ infinitepipes/solver.rs
@@ -6,14 +6,44 @@ const ALL: u8 = UP + RIGHT + DOWN + LEFT;
 
 
 fn rot(pipe: u8) -> u8{
-    return (pipe >> 3 | pipe << 1) & 15; 
+    return (pipe >> 3 | pipe << 1) & 15;
 }
 
 fn h_match(pipe1: u8, pipe2: u8) -> u8 {
     return !(pipe1 ^ pipe2 >> 2) & 2;
 }
 
+fn v_match(pipe1: u8, pipe2:u8) -> u8 {
+    return !(pipe1 >> 2 ^ pipe2) & 1;
+}
+
+fn mismatch() {
+    //iterate through
+}
+
+struct Grid {
+    cols: i32,
+    rows: i32,
+    data: Vec<u8>
+}
+
+impl Grid {
+    fn new(cols: i32, rows: i32) -> Grid {
+        Grid {
+            cols: cols,
+            rows: rows,
+            data: vec![0.0; cols * rows]
+        }
+    }
+}
+
 fn main() {
+    let mut grid_test: [[i32; 4]; 3] = [[1,2,3,4];[1,2,3,4];[1,2,3,4]];
+    println!("grid_test: {}", grid_test);
+    println!("grid at 0,0: {}", grid_test[0][0];
+    (grid_test.0).0 = 4;
+    println!("grid at 0,0: {}", grid_test[0][0];
     println!("{}", rot((LEFT+RIGHT)));
     println!("{}", h_match(LEFT, UP+DOWN));
 }
+             
--- readme.md
+++
@@ -1,8 +0,0 @@
-Man'o'War
-========================
-This is my central repository for /r/dailyprogrammer, code-golfing challenges, and other coding puzzles I find neat. The answers will be written in a variety of langauges, but probably Rust/Python/JS.
-
-Source | Folder | Language | Date 
- --- | --- | --- | ---
-[Calculating De Bruijin Sequences](https://www.reddit.com/r/dailyprogrammer/comments/4riubi/20160706_challenge_274_intermediate_calculating/) | [debruijn](debruijn) | Rust | 2016/7/7
-[Infinite Pipe Solver](https://www.reddit.com/r/dailyprogrammer/comments/4rug59/20160708_challenge_274_hard_loop_solver/) | [infinitepipes](infinitepipes) | Rust | 2016/7/10
