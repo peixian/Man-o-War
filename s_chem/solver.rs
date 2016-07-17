4597fa58370ba4871efb4718df2c5d0ca9fc16fd
Peixian Wang <peixian@fastmail.com>
2016-07-16 19:58:42 -0500

    Finish the alphabetical sort, need to do multi-char for the chem

--- s_chem/solver.rs
+++ s_chem/solver.rs
@@ -1,8 +1,14 @@
+use std::ascii::AsciiExt;
+
+static ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                               'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                               's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 fn verify(element: &str, sym: &str) -> bool{
     let symbols: Vec<String> = get_symbols(element);
     println!("{:?}", symbols);
     for possible_symbol in &symbols {
-        if possible_symbol == sym{
+        if possible_symbol == sym {
             return true;
         }
     }
@@ -24,12 +30,39 @@ fn get_symbols(element: &str) -> Vec<String> {
     return symbols
 }
 
-fn alphabetical_symbol(symbols: &Vec<String>) -> String {
+fn alphabet_loc(letter: char) -> u8 {
+    let ll = letter.to_ascii_lowercase();
+    for i in 0..26 {
+        if ALPHABET[i] == ll {
+            return i as u8;
+        }
+    }
+    //shouldn't ever happen
+    return 0;
 }
 
-fn get_multichar_symbols(element: &str, char_count: &i32) -> Vec<String> {
+fn alphabetical_symbol(symbols: Vec<String>) -> Vec<String> {
+    let mut alphabetical_symbols: Vec<String> = Vec::new();
+    for symbol in symbols {
+        let symbol_chars: Vec<char> = symbol.chars().collect();
+        let symbol_locs: Vec<_> = symbol_chars.iter().map(|&x| alphabet_loc(x)).collect();
+        if symbol_locs[0] < symbol_locs[1] {
+            alphabetical_symbols.push(symbol);
+        }
+    }
+    return alphabetical_symbols;
 }
+
+fn alphabetical(element: &str) -> Vec<String> {
+    let symbols: Vec<String> = get_symbols(element);
+    let alphabetical_symbols: Vec<String> = alphabetical_symbol(symbols);
+    return alphabetical_symbols;
+}
+
+//fn get_multichar_symbols(element: &str, char_count: &i32) -> Vec<String> {
+//}
 
 fn main() {
     println!("{}", verify("Zuulon", "uu"));
+    println!("{:?}", alphabetical("Zuulon"))
 }
